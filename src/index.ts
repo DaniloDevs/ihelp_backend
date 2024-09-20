2cf6ddc85f1546e260ec4e6c80cc4d275722ba90
Danilo Romão <htpps.sw@gmail.com>
2024-09-20 17:15:57 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import fastify from "fastify";
 import { CreateUser } from "./routes/createUser";
 import { FindUserById } from "./routes/findUserById";
-import { exit } from 'node:process'
+import { exit } from 'node:process';
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 import { errorHandler } from "./_error/error-handler";
 
-export const server = fastify()
+export const server = fastify();
 
 
 try {
@@ -15,25 +15,25 @@ try {
 
      // Helper
      server.get("/", (request, reply) => {
-          reply.code(200).send("Server Running")
-     })
+          reply.code(200).send("Server Running");
+     });
 
-     // Rotas
-     server.register(CreateUser)
-     server.register(FindUserById)
-
+     // Routes
+     server.register(CreateUser);
+     server.register(FindUserById);
 
      // Error Handler
-     server.setErrorHandler(errorHandler)
+     server.setErrorHandler(errorHandler);
 
-     //Server
+     // Start Server
      server.listen({
           port: 3031,
-          host: '0.0.0.0'
+          host: '0.0.0.0',
      }, () => {
-          console.log("Server Running!!")
-     })
+          console.log("Server Running!!");
+     });
 } catch (error) {
-     server.log.error(error)
-     exit(1)
+     server.log.error(error);
+     exit(1);
 }
+
